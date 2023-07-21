04a9cd2eb5fff9901ec8b908ce86659f7ca9fc15
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-20 22:56:06 -0400

    Defined linux vm

--- main.tf
+++ main.tf
@@ -126,3 +126,33 @@ resource "azurerm_network_interface" "public-nic" {
     environment = "demo"
   }
 }
+
+#Create linux vm
+resource "azurerm_linux_virtual_machine" "vm-1" {
+    name = "example-machine"
+    resource_group_name = azurerm_resource_group.publix-demo-rg.name
+    location = azurerm_resource_group.publix-demo-rg.location
+    size = "Standard_B1"
+    admin_username = "adminuser"
+    network_interface_ids = [
+        azurerm_network_interface.public-nic.id
+    ]
+
+    admin_ssh_key {
+        username = "adminuser"
+        public_key = file("./publix-demo-vm.pub")
+    }
+
+    os_disk {
+        caching = "ReadWrite"
+        storage_account_type = "StandardLRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer = "0001-com-ubuntu-server-focal"
+        sku = "20_04-lts"
+        version = "latest"
+    }
+
+}
