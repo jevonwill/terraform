53793be520324aef0bb4c46e4509f41c3749c0d5
Ubuntu <azureuser@ubuntu-1.mvyt5rquh03utorpnoahhznz4d.bx.internal.cloudapp.net>
2023-07-21 02:00:01 +0000

    ran terraform fmt

--- main.tf
+++ main.tf
@@ -1,64 +1,64 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = "=3.0.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
-  } 
+  }
 }
 
 #Configure Azure Provider
 provider "azurerm" {
-    features {}
+  features {}
 }
 
 #Create Publix Demo ResourceGroup
 resource "azurerm_resource_group" "publix-demo-rg" {
-    name = "publix-demo-resources"
-    location = "East US"
-    tags = {
-        environment = "demo"
-    }
+  name     = "publix-demo-resources"
+  location = "East US"
+  tags = {
+    environment = "demo"
+  }
 }
 
 #Create Publix Demo Network
 resource "azurerm_virtual_network" "publix-demo-network" {
-    name = "publix-demo-network"
-    resource_group_name = azurerm_resource_group.publix-demo-rg.name
-    location = azurerm_resource_group.publix-demo-rg.location
-    address_space = ["10.11.0.0/16"]
+  name                = "publix-demo-network"
+  resource_group_name = azurerm_resource_group.publix-demo-rg.name
+  location            = azurerm_resource_group.publix-demo-rg.location
+  address_space       = ["10.11.0.0/16"]
 
-    tags = {
-        environment = "demo"
-    }
+  tags = {
+    environment = "demo"
+  }
 
 }
 
 #Create Publix Demo Subnets
 resource "azurerm_subnet" "public-1" {
-    name = "public-subnet-1"
-    resource_group_name = azurerm_resource_group.publix-demo-rg.name
-    virtual_network_name = azurerm_virtual_network.publix-demo-network.name
-    address_prefixes = ["10.11.1.0/24"]
+  name                 = "public-subnet-1"
+  resource_group_name  = azurerm_resource_group.publix-demo-rg.name
+  virtual_network_name = azurerm_virtual_network.publix-demo-network.name
+  address_prefixes     = ["10.11.1.0/24"]
 }
 
 resource "azurerm_subnet" "private-1" {
-    name = "private-subnet-1"
-    resource_group_name = azurerm_resource_group.publix-demo-rg.name
-    virtual_network_name = azurerm_virtual_network.publix-demo-network.name
-    address_prefixes = ["10.11.2.0/24"]
+  name                 = "private-subnet-1"
+  resource_group_name  = azurerm_resource_group.publix-demo-rg.name
+  virtual_network_name = azurerm_virtual_network.publix-demo-network.name
+  address_prefixes     = ["10.11.2.0/24"]
 }
 
 #Create network security groups
 
 resource "azurerm_network_security_group" "public-access-sg" {
-    name = "public-access-sg"
-    location = azurerm_resource_group.publix-demo-rg.location
-    resource_group_name = azurerm_resource_group.publix-demo-rg.name
+  name                = "public-access-sg"
+  location            = azurerm_resource_group.publix-demo-rg.location
+  resource_group_name = azurerm_resource_group.publix-demo-rg.name
 
-   
-    tags = {
-        environment = "demo"
+
+  tags = {
+    environment = "demo"
   }
 }
 
