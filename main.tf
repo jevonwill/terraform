1826ce2d7ab3d19373f05abaa130e82a8dfc52da
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-20 21:33:20 -0400

    Lowered priority of port 80 rule

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ resource "azurerm_network_security_group" "public-access-sg" {
 #Create network security rules
 resource "azurerm_network_security_rule" "public-access-rule" {
   name                        = "inbound-port-80"
-  priority                    = 100
+  priority                    = 99
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
