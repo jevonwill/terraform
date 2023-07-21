481638f42c6f9822c90ebf57f2cb9e494359f53b
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-20 22:21:29 -0400

    defined NIC for public subnets

--- main.tf
+++ main.tf
@@ -108,3 +108,17 @@ resource "azurerm_public_ip" "vm-1-ip" {
     environment = "demo"
   }
 }
+
+#Create network interface
+resource "azurerm_network_interface" "public-nic-1" {
+  name                = "public-nic-1"
+  location            = azurerm_resource_group.publix-demo-rg.location
+  resource_group_name = azurerm_resource_group.publix-demo-rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.public-1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_allocation = azure_public_ip.vm-1-ip.id
+  }
+}
