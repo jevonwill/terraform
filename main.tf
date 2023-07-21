1eb4c0bface7ab9b3e5c45cb1c548919e311d0d1
jevonwill <jevonwilliamsjr@gmail.com>
2023-07-20 20:39:09 -0400

    Defined subnets

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ resource "azurerm_resource_group" "publix-demo-rg" {
     }
 }
 
+#Create Publix Demo Network
 resource "azurerm_virtual_network" "publix-demo-network" {
     name = "publix-demo-network"
     resource_group_name = azurerm_resource_group.publix-demo-rg.name
@@ -33,3 +34,17 @@ resource "azurerm_virtual_network" "publix-demo-network" {
 
 }
 
+#Create Publix Demo Subnets
+resource "azurerm_subnet" "public-1" {
+    name = "public-subnet-1"
+    resource_group_name = azurerm_resource_group.publix-demo-rg.name
+    virtual_network_name = azurerm_virtual_network.publix-demo-network.name
+    address_prexfixes = ["10.11.1.0/24"]
+}
+
+resource "azurerm_subnet" "private-1" {
+    name = "private-subnet-1"
+    resource_group_name = azurerm_resource_group.publix-demo-rg.name
+    virtual_network_name = azurerm_virtual_network.publix-demo-network.name
+    address_prexfixes = ["10.11.2.0/24"]
+}
